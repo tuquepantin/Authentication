9c5e1104c930d9bc024de21d9d178ac005f88819
Victor Pantin <125493189+tuquepantin@users.noreply.github.com>
2023-08-25 03:38:42 +0000

    front

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,63 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+  const [login, setLogin] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = ({ target }) => {
+    setLogin({
+      ...login,
+      [target.name]: target.value,
+    });
+  };
+
+  const sendLogin = (data) => {
+    console.log(data);
+    actions.getLogin(data);
+  };
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="card p-3" style={{ width: "25rem" }}>
+          <form>
+            <div className="form-group">
+              <input
+                className="form-control"
+                placeholder="email"
+                type="text"
+                onChange={handleChange}
+                name="email"
+              />
+            </div>
+            <div className="form-group">
+              <input
+                className="form-control"
+                placeholder="password"
+                type="password"
+                onChange={handleChange}
+                name="password"
+              />
+            </div>
+            <div className="form-group">
+              <button
+                className="btn btn-primary"
+                type="button"
+                onClick={() => sendLogin(login)}
+              >
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
